ba50f8e41d8da1e11f526787836a02101fdb091d
curlynux <smpunchack@gmail.com>
2017-11-06 17:33:24 +0100

    remove deplicayed sub git repo

--- PISCINE_C_CURLYNUX/j04/ex01/ex01/libft/ft_lstdelone.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-#include <stdlib.h>
-
-void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
-{
-	del((*alst)->content, (*alst)->content_size);
-	free(*alst);
-	*alst = NULL;
-}
